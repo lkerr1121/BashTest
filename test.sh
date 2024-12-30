5b21d91432a997974d84afd9f0d13a0ce3a54577
lkerr1121 <lkerr112@gmail.com>
2024-12-30 07:21:11 +0000

    added greeting code

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+first_greeting="Welcome to the bash"
+later_greeting="Welcome back to the bash"
+greeting_occasion=0
 
-echo "Hello World!"
+echo "How many times should I greet you?"
+
+read greeting_occasion
+
+	if [ $greeting_occasion -lt 1 ] 
+	then 
+		echo $first_greeting
+	else
+		echo $later_greeting
+	fi
